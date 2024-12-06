c0e95a2eb5cb9fc5e1dfec256c26e6ef2e0d04d3
teja8328 <135217705+teja8328@users.noreply.github.com>
2024-12-06 15:28:08 +0530

    Update connection.py

--- app/database/connection.py
+++ app/database/connection.py
@@ -2,6 +2,6 @@ import os
 from urllib.parse import quote_plus
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Makonis@localhost:5432/algo_project'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:mti@167.71.235.134:5432/mtidb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'default_secret_key')
